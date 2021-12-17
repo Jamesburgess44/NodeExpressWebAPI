c74c75bb5e1b307ec64595b8bb23920f280792af
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-12-17 14:55:01 -0700

    added config file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 mongoose
-    .connect('mongodb+srv://jamesb:Water123@jamesdcc.l5s3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    .connect('mongoURI',
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB... Cowabunga Dude!'))
     .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
