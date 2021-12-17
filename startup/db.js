a5a12acb5f1771cb4eda5b28970f339e93c0772b
Jamesburgess44 <jamesburgess44@yahoo.com>
2021-12-17 15:17:44 -0700

    added product model

---
+++ startup/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+function connectDB() {
+    mongoose.connect(
+        config.get('mongoURI'),
+        { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connected to MongoDB... Cowabunga Dude!'))
+        .catch((err) => { console.log(`Could not connect to MongoDB. ERROR: ${err}`);
+        process.exit(1);
+        });
+    
+}
+    
+module.exports = connectDB;
